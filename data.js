50b651802860917c42ca1ecf4ff09b4fef6f05f4
Jorge Orenos <orenosjorge@gmail.com>
2026-03-02 10:39:14 -0600

    First commit

---
+++ data.js
@@ -0,0 +1,14 @@
+const newsData = [
+    // FILA 1
+    { fecha: "Fecha", resumen: "Agrgar contenido.", fila: 1 },
+
+    // FILA 2
+    { fecha: "Fecha", resumen: "Agregar contenido.", fila: 2 },
+
+    // FILA 3
+    { fecha: "Fecha", resumen: "Agregar contenido.", fila: 3 },
+
+    // FILA 4
+    { fecha: "Fecha", resumen: "Agregar contenido.", fila: 4 },
+
+];
