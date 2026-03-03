c9ee725a840c3ea538e8cc62f6c619e6f3cc37fc
Jorge Orenos <orenosjorge@gmail.com>
2026-03-03 16:48:03 -0600

    chore: updated march 3, 2026

--- data.js
+++ data.js
@@ -1,14 +1,27 @@
 const newsData = [
     // FILA 1
-    { fecha: "Fecha", resumen: "Agrgar contenido.", fila: 1 },
+    { fecha: "1941", resumen: "A inicios de los 40's Irán se encontraba ocupada por las fuerzas aliadas. Aunque Estados Unidos y Gran Bretaña se retiraron al finalizar la segunda guerra mundial, no lo hizo de esta manera la Unión Soviética.", fila: 1 },
+    { fecha: "1946", resumen: "Los iraníes pro soviéticos comenzaron una guerra interna, la que se conoció como la crisis de Irán de 1946. Ello terminó con la proclamación de la separatista República Popular de Azerbaiyán.", fila: 1 },
+    { fecha: "1946-1951", resumen: "Finalizada la guerra, una coalición de nacionalistas, clérigos y partidos de izquierda no comunistas, se unió bajo Mohammad Mosaddegh, quien deseaba reducir el poder la monarquía y el clero en Irán.", fila: 1 },
+    { fecha: "1951", resumen: "Mohammad Mosaddegh se convierte en Primer Ministro de Irán e inmediatamente nacionaliza el petróleo. En ese momento Gran Bretaña era el principal benefactor del petróleo iraní e impuso un embargo económico a Irán.", fila: 1 },
+    { fecha: "1953", resumen: "Internamente el apoyo a Mosaddegh se debilitó por el bajo desempeño de sus políticas sociales y económicas. En agosto de 1953 huye del país debido a un golpe de estado financiado por la CIA.", fila: 1 },
+    { fecha: "1954", resumen: "Con la caída de Mohammad Mosaddegh finaliza la nacionalización del petróleo iraní. En 1954 un consorcio multinacional liderado por 'British Petroleum' acelera el desarrollo petrolero iraní.", fila: 1 },    
 
     // FILA 2
-    { fecha: "Fecha", resumen: "Agregar contenido.", fila: 2 },
+    { fecha: "1960-1963", resumen: "Durante este periodo, Irán experimentó cambios políticos y económicos significativos. Entre ellos destacan la modernización industrial y el crecimiento económico impulsado por los ingresos petroleros. Además, existieron grandes avances en materia de derechos humanos y de la mujer.", fila: 2 },
+    { fecha: "1963", resumen: "Comienza a manifestarse resistencia contra las reformas de sha, Mohammad Reza Pahlavi, especialmente dentro de la comunidad chiita. Es en este contexto la figura del ayatolá Ruhollah Khomeini comienza a ganar popularidad pese a su exilio.", fila: 2 },
+    { fecha: "1970-1975", resumen: "Durante este quinquenio el sha impulsó una serie de reformas económicas que culminaron en una crisis económica, acelerando el descontento hacia su figura. Por otro lado Khomeini vuelve a ser relevante en el ámbito social y político, y muchos argumentan que Irán debe regresar a su cultura autóctona (clerical).", fila: 2 },
+    { fecha: "1978", resumen: "La sensación de corrupción en el Gobierno iraní exacerbó los ánimos contra el régimen. Es así como en enero de 1978 comienzan una serie de protestas contra el sha, terminado con la muerte muchos de los manifestantes. Lo que solo alimentó el descontento social.", fila: 2 },
+    { fecha: "1978", resumen: "Desde su exilio en Francia, Khomeini coordinó el recrudecimiento de la oposición contra el régimen. En enero de 1979 el sha huye de Irán y muere al año siguiente en Egipto.", fila: 2 },
+    { fecha: "1979", resumen: "El 1 de abril, con el apoyo del referéndum nacional, Khomeini declaró a Irán como una república islámica, imponiendo un retorno a los valores sociales conservadores. Además, creó la Guardia Revolucionaria con el propósito de reprimir cualquier golpe de Estado.", fila: 2 },
 
     // FILA 3
-    { fecha: "Fecha", resumen: "Agregar contenido.", fila: 3 },
-
-    // FILA 4
-    { fecha: "Fecha", resumen: "Agregar contenido.", fila: 4 },
+    { fecha: "1989", resumen: "Después de un periodo de guerra contra Irak por disputas territoriales, el ayatolá Khomeini muere en enero de este año y comienzan los preparativos para su sucesión. El elegido fue Alí Khamenei quien fue el líder supremo de Irán desde esta fecha hasta su muerte en 2026.", fila: 3 },
+    { fecha: "1989-2001", resumen: "Comenzó un periodo de declive en las relaciones entre Estados Unidos e Irán. Principalmente debido a que Irán se oponía a las invasiones estadounidenses en la región, en especial después de los ataques del 11 de septiembre de 2001.", fila: 3 },
+    { fecha: "2002", resumen: "El programa nuclear iraní acaparó la atención internacional cuando un grupo disidentes al régimen reveló que Irán estaba construyendo en secreto una planta de enriquecimiento de uranio. Aunque Irán insistió en que sus actividades tenían fines pacíficos.", fila: 3 },
+    { fecha: "2006", resumen: "El Consejo de Seguridad de la ONU impuso las primeras sanciones dirigidas al programa nuclear.", fila: 3 },
+    { fecha: "2015", resumen: "Una coalición integrada por Estados Unidos, China, Rusia, Francia, Alemania y Reino Unido, alcanzaron un acuerdo junto a Irán para restringir y supervisar su programa nuclear.", fila: 3 },
+    { fecha: "2018", resumen: "El presidente de los Estados Unidos, Donald Trump, anunció la salida de Estados Unidos del acuerdo previamente alcanzado y afirmó que reanudaría las sanciones económicas contra Irán.", fila: 3 },
+    { fecha: "2025-2026", resumen: "Estados Unidos e Israel lanzan multiples ataques contra Irán, alegando que Irán se encuentra cerca del desarrollo de la bomba nuclear. Los últimos de los ataques han dado como resultado la muerte de Khamenei una escalada en el conflicto entre ambas partes.", fila: 3 },
 
 ];
